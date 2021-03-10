94c8feac0d1d05f4dc28bf97fe1cae9a114d408d
nidhi <nidhi.priya17@gmail.com>
2021-03-10 15:28:28 +0530

    new cart feature added

---
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -0,0 +1,13 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class CartPage {
+	WebDriver driver;
+	
+	
+	public CartPage(WebDriver driver) {
+		this.driver= driver;
+	}
+
+}
